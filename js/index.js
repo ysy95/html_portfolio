eaf923e72a7485a7b4b485f2596879a9a13aa054
ysy95 <tptlffldk21@naver.com>
2024-06-21 19:35:34 +0900

    'top_btn'

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+// 탑버튼 스크립트
+let Top = document.getElementById('top_btn');
+
+window.addEventListener('scroll',function(){
+  if(this.scrollY >200){
+    Top.classList.add('on');
+  }else{
+    Top.classList.remove('on');
+  }
+});
+
+// 부드럽게 맨위로 올라가기
+Top.addEventListener('click',function(e){
+  e.preventDefault();
+  window.scrollTo({top:0, behavior:'smooth'});
+});
